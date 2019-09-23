801bf673fa68bc131f39a182ea6ab73c1891c508
Sarvansh Prasher <sarvansh.p@collpoll.com>
2019-09-23 13:19:51 -0700

    Added app folder and made changes in package.json file

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const path = require('path')
 function createWindow() {
    win = new BrowserWindow({width: 1000, height: 800})
    win.loadURL(url.format ({
-      pathname: path.join(__dirname, 'index.html'),
+      pathname: path.join(__dirname, 'app/index.html'),
       protocol: 'file:',
       slashes: true
    }))
