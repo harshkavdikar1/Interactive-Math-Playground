676e3ea92c1b2b1c970cb401f4d6e0d30e4f0098
Sarvansh Prasher <sarvansh.p@collpoll.com>
2019-09-24 18:00:04 -0700

    Added package for storing db json files and added code for encryption and decryption of data

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const {app, BrowserWindow} = require('electron')
 const url = require('url')
 const path = require('path')
+const crypto = require('crypto');
+const algorithm = 'aes-256-cbc';
 
 function createWindow() {
    win = new BrowserWindow({width: 1000, height: 800})
@@ -9,6 +11,65 @@ function createWindow() {
       protocol: 'file:',
       slashes: true
    }))
+
+
+function encrypt(text){
+
+  let cipher = crypto.createCipheriv(algorithm,Buffer.from(key),iv);
+
+  let encrypted = cipher.update(text);
+
+  encrypted = Buffer.concat([encrypted , cipher.final()]);
+
+  return {iv: iv.toString('hex'),encryptedData:encrypted.toString('hex')};
+
 }
+
+
+function decrypt(text){
+
+  let iv = Buffer.from(text.iv, 'hex');
+
+  let encryptedText = Buffer.from(text.encryptedData,'hex')
+
+  let decipher = cyrpto.createDecipheriv('aes-256-cbc', Buffer.from(key),iv);
+
+  decrypted = Buffer.concat([decrypted,decipher.final()]);
+
+  return decrypted.toString();
+}
+
+var hw = encrypt("sarvanshprasher");
+console.log(hw);
+
+console.log(decrypt(hw));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.on('ready', createWindow)
