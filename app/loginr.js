a8513537bb8d29cf8f64d1236590bbfa5bda2084
harshkavdikar1 <harshkavdikar1@gmail.com>
2019-10-20 02:47:36 -0700

    US#52 Task#55

--- app/loginr.js
+++ app/loginr.js
@@ -1,9 +1,11 @@
-const electron=require('electron')
-const ipc=electron.ipcRenderer
+const electron = require('electron')
+const ipc = electron.ipcRenderer
 const BrowserWindow = electron.remote.BrowserWindow;
-const submit_btn=document.getElementById('submit')
-const email_txt=document.getElementById('email_id')
-const password_txt=document.getElementById('password')
+const submit_btn = document.getElementById('submit')
+const name = document.getElementById('name')
+const age = document.getElementById('age')
+const email_txt = document.getElementById('email_id')
+const password_txt = document.getElementById('password')
 
 
 submit_btn.addEventListener('click',function(event)
@@ -14,21 +16,5 @@ submit_btn.addEventListener('click',function(event)
 })
 
 function takeToSignUp(){
-
-	// Create the browser window.
-  win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
-  // and load the login.html of the app.
-  win.loadURL('file://'+__dirname+'/signup.html')
-  // Open the DevTools.
-  win.webContents.openDevTools()
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    win = null
-  })
+	location.href = "signup.html"
 }
