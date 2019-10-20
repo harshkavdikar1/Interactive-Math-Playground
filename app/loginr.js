b83579b31e6e0fedb2c61fb9ac0f29c078707148
harshkavdikar1 <harshkavdikar1@gmail.com>
2019-10-20 03:42:46 -0700

    US#52 Task#59 part 1

--- app/loginr.js
+++ app/loginr.js
@@ -1,20 +1,32 @@
 const electron = require('electron')
 const ipc = electron.ipcRenderer
-const BrowserWindow = electron.remote.BrowserWindow;
 const submit_btn = document.getElementById('submit')
 const name = document.getElementById('name')
 const age = document.getElementById('age')
-const email_txt = document.getElementById('email_id')
-const password_txt = document.getElementById('password')
+const email = document.getElementById('email_id')
+const password = document.getElementById('password')
 
 
 submit_btn.addEventListener('click',function(event)
 {
 	//sending data to main process.
-	ipc.send('sent_credentials',email_txt.value,password_txt.value)
-
+	if (email.value != '' && password.value != ''){
+		ipc.send('login_credentials',email.value,password.value)
+	}
+	else if (name.value == '' || age.value == '') {
+		alert('Please Enter Name and Age or Email and Password to login');
+	}
+	else if (age.value < 8) {
+		location.href = "home.html";
+	}
+	else if (age.value < 12) {
+		location.href = "home2.html";
+	}
+	else {
+		location.href = "home3.html";
+	}
 })
 
 function takeToSignUp(){
-	location.href = "signup.html"
+	location.href = "signup.html";
 }
